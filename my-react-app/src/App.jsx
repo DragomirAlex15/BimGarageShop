bfb8fd0cc63a841b960003dc8a341fc227b4b189
Dragomir Alexandru <dragomiralexandru153@gmail.com>
2025-10-21 16:16:40 +0300

    Home pages work
    -video dosen't have correct  size

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,16 @@
-import "./App.css"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
-import Home from "./pages/Home"
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
       <Navbar />
       <Home />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
